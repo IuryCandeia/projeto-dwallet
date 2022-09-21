0f77eb9f7c18206e079c5ad778d64afb17b72c16
Iury <iurymorais@ads.fiponline.edu.br>
2022-09-21 11:18:06 -0300

    Change name project para Usuario

--- dwallet/urls.py
+++ dwallet/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path
-from project.views import home, store, cadastro
+from Usuario.views import home, store, cadastro
 
 urlpatterns = [
     path('admin/', admin.site.urls),
