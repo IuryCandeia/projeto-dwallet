394b539fcbbf3592b9ffdb23aa6e992982da0aad
Iury <iurymorais@ads.fiponline.edu.br>
2022-09-28 15:46:14 -0300

    att

--- Usuario/models.py
+++ Usuario/models.py
@@ -1,21 +1,22 @@
+from email.policy import default
 from statistics import mode
 from django.contrib.auth.models import User
 from django.db import models
 
 class Usuario(models.Model):
-    nome = models.CharField(max_length=150)
-    numero = models.CharField(max_length=15)
-    User.email = models.CharField(max_length=50)
+    User.nome = models.CharField(max_length=150, default='')
+    numero = models.CharField(max_length=15, default='')
+    User.email = models.CharField(max_length=50, default='')
     
     def __str__(self):
         return self.nome
 
 
 class Financa(models.Model):
-    titulo = models.CharField(verbose_name="Título", max_length=40)
-    tipo = models.CharField(verbose_name="Tipo", max_length=20, default="Mensal")
-    descricao = models.TextField(verbose_name="Descrição", max_length=250)
-    valor = models.FloatField(verbose_name="Valor", max_length=20, default="0,0")
+    titulo = models.CharField("Título", max_length=40)
+    tipo = models.CharField("Tipo", max_length=20, default="Mensal")
+    descricao = models.TextField("Descrição", max_length=250)
+    valor = models.FloatField("Valor", max_length=20, default="0,0")
 
     def __str__(self):
         return "{} - {} - {} ({})".format(self.titulo, self.tipo, self.descricao, self.valor)
